5943ef478918ca9ef7582fe3638bff2d0c67f2fb
evilevgeniys <903jenya@gmail.com>
2024-04-23 14:51:24 +0300

    начало проекта

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+    return(
+        <div className='app'>
+            <div class="container">
+                <div className='main'>
+                    <div className="main__title">console.log (<span>Hello, world!</span>) <br></br> Меня зовут Евгений Игнатюк</div>
+                    <div className="main__subtitle">Я <span>web</span>-разработчик <br></br>из города Бреста</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
