5c39a257fe243705f2f547a11c2456f5795ffba1
evilevgeniys <903jenya@gmail.com>
2024-05-29 16:22:26 +0300

    переделка концепции проекта

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import '.header.scss';
+import {NavBar, Nav, Container} from 'react-bootstrap';
+
+const Header = () => {
+    
+    return(
+        <NavBar>
+            <Container>
+                
+            </Container>
+        </NavBar>
+    )
+}
+
+export default Header;
