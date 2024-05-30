f824964d44846c885db95926692b57040d44db1c
evilevgeniys <903jenya@gmail.com>
2024-05-30 21:25:50 +0300

    начат главный экран

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import Header from "../header/Header";
+import MainPage from "../mainPage/MainPage";
 
 const App = () => {
 	return (
 		<div className="app">
 			<Header/>
+			<MainPage/>
 		</div>
 	);
 };
--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,9 +1,18 @@
+import { Container } from "react-bootstrap";
 import "./MainPage.scss";
 
 const MainPage = () => {
     return(
        <section className="mainPage">
-            
+            <Container>
+                <div className="mainPage__title col-md-7">
+                    <h1 className="mainPage__title-upper">Hello, world! I'm Evgenij</h1>
+                    <h2 className="mainPaage__title-low">A beginner <br/> <span className="mainPage_text-span">{'< frontend developer >'} </span></h2>
+                </div>
+                <div className="mainPage__photo fade-in col-md-5">
+
+                </div>
+            </Container>
        </section>
     );
 }
