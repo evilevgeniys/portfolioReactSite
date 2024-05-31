d9b400115291694d36443dee1d5b052ff5c74c05
evilevgeniys <903jenya@gmail.com>
2024-05-31 16:21:53 +0300

    закончена секция главного экрана

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,18 +1,21 @@
-import { Container } from "react-bootstrap";
 import "./MainPage.scss";
+import mainPhoto from '../../images/main_photo.PNG';
 
 const MainPage = () => {
     return(
        <section className="mainPage">
-            <Container>
-                <div className="mainPage__title col-md-7">
-                    <h1 className="mainPage__title-upper">Hello, world! I'm Evgenij</h1>
-                    <h2 className="mainPaage__title-low">A beginner <br/> <span className="mainPage_text-span">{'< frontend developer >'} </span></h2>
+            <div className="container">
+                <div className="row">
+                    <div className="mainPage__title col-md-7">
+                        <h1 className="mainPage__title-upper">Hello, world!<br></br>I'm Evgenij</h1>
+                        <h2 className="mainPaage__title-low">A beginner <br/> <span className="mainPage_text-span">{'< frontend developer />'} </span></h2>
+                    </div>
+                    <div className="mainPage__photo fade-in col-md-5">
+                        <img src={mainPhoto} alt=""></img>
+                    </div>
                 </div>
-                <div className="mainPage__photo fade-in col-md-5">
-
-                </div>
-            </Container>
+                <div className=""></div>
+            </div>
        </section>
     );
 }
