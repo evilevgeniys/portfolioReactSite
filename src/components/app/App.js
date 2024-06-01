7ce3f587aa3a342d7479e765b09a723ec7a5b05a
evilevgeniys <903jenya@gmail.com>
2024-06-01 10:22:13 +0300

    dayle update

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import Header from "../header/Header";
 import MainPage from "../mainPage/MainPage";
+import SocialLinks from "../SocialLInks/SocialLinks";
 
 const App = () => {
 	return (
 		<div className="app">
 			<Header/>
 			<MainPage/>
+			<SocialLinks/>
 		</div>
 	);
 };
