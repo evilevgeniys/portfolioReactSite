769a710d27d57bdabe94ceb77fdf0cfd0f2574b8
evilevgeniys <903jenya@gmail.com>
2024-06-04 20:21:24 +0300

    minor fixes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,18 @@
 import Header from "../header/Header";
 import MainPage from "../mainPage/MainPage";
 import SocialLinks from "../SocialLInks/SocialLinks";
+import AboutMe from "../aboutMe/AboutMe";
+import { Container } from "react-bootstrap";
 
 const App = () => {
 	return (
 		<div className="app">
-			<Header/>
-			<MainPage/>
-			<SocialLinks/> // added random comment
+			<Container>
+				<Header/>
+				<MainPage/>
+				<AboutMe/>
+			</Container>
+			<SocialLinks/> 
 		</div>
 	);
 };
