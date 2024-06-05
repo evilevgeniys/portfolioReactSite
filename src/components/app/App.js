6d8fbd205c61af92d38837aad5f94d802efee98b
evilevgeniys <903jenya@gmail.com>
2024-06-05 15:39:57 +0300

    добавлен стиль для span

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,6 @@ const App = () => {
 			<Container>
 				<Header/>
 				<MainPage/>
-				<AboutMe/>
 			</Container>
 			<SocialLinks/> 
 		</div>
--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -8,15 +8,15 @@ const MainPage = () => {
             <div className="container">
                 <div className="row">
                     <div className="mainPage__title col-md-7">
-                        <h1 className="mainPage__title-upper">Hello, world!<br></br>I'm Evgenij</h1>
-                        <h2 className="mainPaage__title-low">A beginner <br/> <span className="mainPage_text-span">{'< frontend developer />'} </span></h2>
+                        <h1 className="mainPage__title-upper">Hello, world!<br></br>Я, Евгений</h1>
+                        <h2 className="mainPaage__title-low">Начинающий  <br/> <span className="mainPage_text-span">{'< frontend разработчик />'} </span></h2>
                     </div>
                     <div className="mainPage__photo fade-in col-md-5">
                         <img src={mainPhoto} alt=""></img>
                     </div>
                 </div>
                 <div className="row">
-                    
+                    <AboutMe/>
                 </div>
             </div>
        </section>
