597d66984742df4399fa93af9fa5ca990228b9f0
evilevgeniys <903jenya@gmail.com>
2024-06-11 16:22:15 +0300

    Создан шаблон карточки проекта

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -1,12 +1,4 @@
 
-//stack img list:
-// {name: "HTML5", icon: ""},
-// {name: "CSS3", icon: ""},
-// {name: "JS", icon: ""},
-// {name: "SASS", icon: ""},
-// {name: "ReactJS", icon: ""},
-// {name: "Bootstrap", icon: ""},
-
 export const PROJECTS = [
 	{
 		id: "0",
@@ -16,6 +8,13 @@ export const PROJECTS = [
 		githubUrl: "https://github.com/evilevgeniys",
 		gitHubIconUrl: "",
 		imageUrl: "",
-		stack: []
+		stack: [
+			{name: "HTML5", icon: ""},
+			{name: "CSS3", icon: ""},
+			{name: "JS", icon: ""},
+			{name: "SASS", icon: ""},
+			{name: "ReactJS", icon: ""},
+			{name: "Bootstrap", icon: ""},
+		]
 	}
 ]
