9932d59800bbeb72a9b880edfe43819c3c2f568c
evilevgeniys <903jenya@gmail.com>
2024-06-11 15:50:46 +0300

    Добавлен шаблон списка проектов для страницы Портфолио

---
+++ src/utils/Projects.js
@@ -0,0 +1,21 @@
+
+//stack img list:
+// {name: "HTML5", icon: ""},
+// {name: "CSS3", icon: ""},
+// {name: "JS", icon: ""},
+// {name: "SASS", icon: ""},
+// {name: "ReactJS", icon: ""},
+// {name: "Bootstrap", icon: ""},
+
+export const PROJECTS = [
+	{
+		id: "0",
+		title: "TEST",
+		description: "This is test card in my portfolio",
+		websiteUrl: "",
+		githubUrl: "https://github.com/evilevgeniys",
+		gitHubIconUrl: "",
+		imageUrl: "",
+		stack: []
+	}
+]
